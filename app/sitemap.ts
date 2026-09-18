6218a49808fb48a4e3e4f1fbbecabec4594fbf7a
Alfaiz-on <faiz80824@gmail.com>
2026-09-18 19:17:33 +0500

    sitemap updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,52 +5,52 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
 
     {
-      url: "https://fixonic-solution.vercel.app",
+      url: "https://fixonicsolutions.com/",
       lastModified: new Date(),
     },
 
     {
-      url: "https://fixonic-solution.vercel.app/about",
+      url: "https://fixonicsolutions.com/about",
       lastModified: new Date(),
     },
 
     {
-      url: "https://fixonic-solution.vercel.app/contact",
+      url: "https://fixonicsolutions.com/contact",
       lastModified: new Date(),
     },
 
     {
-      url: "https://fixonic-solution.vercel.app/services",
+      url: "https://fixonicsolutions.com/services",
       lastModified: new Date(),
     },
 
     {
-      url: "https://fixonic-solution.vercel.app/services/website-development",
+      url: "https://fixonicsolutions.com/services/website-development",
       lastModified: new Date(),
     },
 
     {
-      url: "https://fixonic-solution.vercel.app/services/seo",
+      url: "https://fixonicsolutions.com/services/seo",
       lastModified: new Date(),
     },
 
     {
-      url: "https://fixonic-solution.vercel.app/services/digital-marketing",
+      url: "https://fixonicsolutions.com/services/digital-marketing",
       lastModified: new Date(),
     },
 
     {
-      url: "https://fixonic-solution.vercel.app/services/ui-ux-design",
+      url: "https://fixonicsolutions.com/services/ui-ux-design",
       lastModified: new Date(),
     },
 
     {
-      url: "https://fixonic-solution.vercel.app/services/ai-solutions",
+      url: "https://fixonicsolutions.com/services/ai-solutions",
       lastModified: new Date(),
     },
 
     {
-      url: "https://fixonic-solution.vercel.app/services/domain-hosting",
+      url: "https://fixonicsolutions.com/services/domain-hosting",
       lastModified: new Date(),
     },
 
